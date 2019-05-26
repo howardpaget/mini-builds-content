0dbcf5a2f14cf5107ec8914b952f8c3af307f66c
Howard Paget <9295596+howardpaget@users.noreply.github.com>
2019-05-26 10:59:05 +0100

    Add python v2 version of deploy

---
+++ deploy_entries_pyv2.py
@@ -0,0 +1,31 @@
+import sqlite3
+import os
+import json
+from os.path import expanduser
+home = expanduser("~")
+print(str(home))
+
+entries_dirs = os.listdir("entries")
+entries = []
+categories = []
+
+for entry in entries_dirs:
+    body = open('entries/' + entry + '/body.md', 'r').read()
+    metadata = json.load(open('entries/' + entry + '/metadata.json', 'r'))
+    print(metadata['title'])
+
+    entries.append((metadata['id'], metadata['title'], metadata['snippet'], body, metadata['date'], metadata['category'], ''))
+    categories.append((metadata['category'], metadata['category'].title()))
+
+conn = sqlite3.connect(str(home) + '/.picoblog/picoblog.db')
+
+c = conn.cursor()
+
+c.executemany('INSERT OR REPLACE INTO Entry VALUES (?, ?, ?, ?, ?, ?, ?)', entries)
+
+conn.commit()
+
+c.executemany('INSERT OR REPLACE INTO Category VALUES (?, ?)', categories)
+c.close()
+conn.commit()
+conn.close()
